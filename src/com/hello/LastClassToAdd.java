e2d795dfcb5790ad317511bf2436e8017cc2c04f
shiromarzai <shahryar_omarzai@hotnmail.com>
2023-05-20 11:43:36 -0700

    Last class to add

--- src/com/hello/LastClassToAdd.java
+++ src/com/hello/LastClassToAdd.java
@@ -1,10 +1,10 @@
 package com.hello;
 
-public class LastClassToPush {
+public class LastClassToAdd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Last class to Add");
 	}
 
 }

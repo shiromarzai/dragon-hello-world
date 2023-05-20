f750105ee46da001b29ab315cec36353250ed05a
shiromarzai <shahryar_omarzai@hotnmail.com>
2023-05-20 11:32:58 -0700

    Added Another Class

---
+++ src/com/hello/AnotherJavaClass.java
@@ -0,0 +1,11 @@
+package com.hello;
+
+public class AnotherJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My File Added");
+
+	}
+
+}
